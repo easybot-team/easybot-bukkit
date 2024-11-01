91b34c34b3c37e213e374624288fa7f83082d25e
zkhssb <z164907681@gmail.com>
2024-11-01 19:49:03 +0800

    feat: 弓箭击杀者获取

--- easybot_bukkit/src/main/java/com/springwater/easybot/event/PlayerDeathSyncEvents.java
+++ easybot_bukkit/src/main/java/com/springwater/easybot/event/PlayerDeathSyncEvents.java
@@ -4,6 +4,7 @@ import com.springwater.easybot.Easybot;
 import com.springwater.easybot.bridge.packet.PlayerInfoWithRaw;
 import com.springwater.easybot.utils.BridgeUtils;
 import org.bukkit.block.Block;
+import org.bukkit.entity.Arrow;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -19,6 +20,13 @@ public class PlayerDeathSyncEvents implements Listener {
         EntityDamageEvent lastDamageCause = player.getLastDamageCause();
         if (lastDamageCause instanceof EntityDamageByEntityEvent) {
             Entity damager = ((EntityDamageByEntityEvent) lastDamageCause).getDamager();
+            if(damager instanceof Arrow){
+                Arrow arrow = (Arrow) damager;
+                if(arrow.getShooter() instanceof Entity){
+                    return ((Entity) arrow.getShooter()).getName();
+                }
+                return "箭";
+            }
             return damager != null ? damager.getName() : "一股神秘的力量";
         } else if (lastDamageCause instanceof EntityDamageByBlockEvent) {
             Block damager = ((EntityDamageByBlockEvent) lastDamageCause).getDamager();
@@ -31,11 +39,16 @@ public class PlayerDeathSyncEvents implements Listener {
     @EventHandler(priority = EventPriority.LOWEST)
     public void onPlayerDeath(PlayerDeathEvent event) {
         PlayerInfoWithRaw playerInfo = BridgeUtils.buildPlayerInfoFull(event.getEntity());
+        String deathMessage = event.getDeathMessage();
+        if(deathMessage == null){
+            deathMessage = event.getEntity().getName() + "  died";
+        }
+        final String message = deathMessage;
         String killer = getKiller(event.getEntity());
         new Thread(() -> {
             Easybot
                     .getClient()
-                    .syncDeathMessage(playerInfo, event.getDeathMessage(), killer);
+                    .syncDeathMessage(playerInfo, message, killer);
         }, "EasyBotThread-SyncPlayerDeath").start();
     }
 }
