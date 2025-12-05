19c76e7287c4cb8c8df27fe1cb8fd50dc9a993f7
zkhssb <z164907681@gmail.com>
2025-12-06 02:15:16 +0800

    breaking: 将项目移动到root,支持自动构建Release

--- easybot_bridge/src/main/java/com/springwater/easybot/bridge/packet/HeartbeatPacket.java
+++
@@ -1,9 +0,0 @@
-package com.springwater.easybot.bridge.packet;
-
-import com.springwater.easybot.bridge.OpCode;
-
-public class HeartbeatPacket extends Packet {
-    public HeartbeatPacket(){
-        setOpCode(OpCode.HeartBeat);
-    }
-}
--- easybot_bridge/src/main/java/com/springwater/easybot/bridge/packet/NeedSyncSettingsPacket.java
+++
@@ -1,7 +0,0 @@
-package com.springwater.easybot.bridge.packet;
-
-public class NeedSyncSettingsPacket extends PacketWithCallBackId{
-    public NeedSyncSettingsPacket(){
-        setOperation("NEED_SYNC_SETTING");
-    }
-}
